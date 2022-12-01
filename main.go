18cf6a649cc2984850583d26da2163bf4c557bb1
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-01 17:20:25 +0100

    Working example on the test data

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("most calories (test data): ", findMostCalories(testData))
 }
 
 func findMostCalories(data string) int {
-	return 123;
+	parts := strings.Split(data, "\n")
+
+	max := 0
+	sum := 0
+
+	for _, s := range parts {
+		if s == "" {
+			max = Max(max, sum)
+			sum = 0
+			
+			fmt.Println("EMPTY")
+		} else {
+			i, _ := strconv.Atoi(s)
+			sum += i
+			fmt.Println(s)
+		}
+	}
+
+	return max
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
 }
