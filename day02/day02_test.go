da3953124636de53d38174fb4d59a2536768ea54
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-02 22:30:20 +0100

    Add tests for day 02 step 2

--- day02/day02_test.go
+++ day02/day02_test.go
@@ -17,3 +17,19 @@ func TestCalculateTotalScore(t *testing.T) {
 		}
 	}
 }
+
+func TestCalculateTotalScoreStep2(t *testing.T) {
+	cases := []struct {
+		in string
+		want int
+	}{
+			{TestData, 12},
+			{Data, 13448},
+	}
+	for _, c := range cases {
+		got := CalculateTotalScoreStep2(c.in)
+		if got != c.want {
+			t.Errorf("CalculateTotalScoreStep2(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
