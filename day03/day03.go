c17ad3681225af81ebb68486e75b13f1b8713cb9
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-03 11:54:08 +0100

    Work on step 2

--- day03/day03.go
+++ day03/day03.go
@@ -16,6 +16,18 @@ func CalculateSumOfPriorities(listOfContents string) int {
 	return sum
 }
 
+func CalculateSumOfPrioritiesStep2(listOfContents string) int {
+
+	rucksacks := strings.Split(listOfContents, "\n")
+
+	sum := 0
+	for i := 0; i < len(rucksacks); i += 3 {
+		sum += findCommonItem(rucksacks[i:i+3])
+	}
+
+	return sum
+}
+
 func calculatePriority(rucksack string) int {
 	sizeOfCompartment := len(rucksack) / 2
 
@@ -24,6 +36,16 @@ func calculatePriority(rucksack string) int {
 
 	for _, item := range compartment1 {
 		if strings.Contains(compartment2, string(item)) {
+			return calculatePriorityOfItem(item)
+		}
+	}
+
+	panic("cannot reach")
+}
+
+func findCommonItem(rucksacks []string) int {
+	for _, item := range rucksacks[0] {
+		if strings.Contains(rucksacks[1], string(item)) && strings.Contains(rucksacks[2], string(item)) {
 			return calculatePriorityOfItem(item)
 		}
 	}
--- day03/day03_test.go
+++ day03/day03_test.go
@@ -13,7 +13,23 @@ func TestCalculateSumOfPriorities(t *testing.T) {
 	for _, c := range cases {
 		got := CalculateSumOfPriorities(c.in)
 		if got != c.want {
-			t.Errorf("CalculateTotalScore(%v) == %v, want %v", c.in, got, c.want)
+			t.Errorf("CalculateSumOfPriorities(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
+
+func TestCalculateSumOfPrioritiesStep2(t *testing.T) {
+	cases := []struct {
+		in string
+		want int
+	}{
+			{TestData, 70},
+			// {Data, 8185},
+	}
+	for _, c := range cases {
+		got := CalculateSumOfPrioritiesStep2(c.in)
+		if got != c.want {
+			t.Errorf("CalculateSumOfPrioritiesStep2(%v) == %v, want %v", c.in, got, c.want)
 		}
 	}
 }
