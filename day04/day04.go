47d659fb474df9d7f211492e1011a6d1b79455bd
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-04 12:16:39 +0100

    Implement day 04

--- day04/day04.go
+++ day04/day04.go
@@ -1,5 +1,52 @@
 package day04
 
-func CalculateOverlappingAssignmentPairs(assignmentPairs string) int {
-	panic("not implemented")
+import (
+	"strconv"
+	"strings"
+)
+
+type sectionAssignment struct {
+	start int
+	end int
+}
+
+func CalculateOverlappingAssignmentPairs(listOfAssignmentPairs string) int {
+	assignmentPairs := strings.Split(listOfAssignmentPairs, "\n")
+
+	countOverlaps := 0
+	for _, pair := range assignmentPairs {
+		if arePairsOverlapping(pair) {
+			countOverlaps++
+		}
+	}
+
+	return countOverlaps
+}
+
+func arePairsOverlapping(assignmentPair string) bool {
+	first, second := parsePair(assignmentPair)
+
+	return isOverlapping(first, second) || isOverlapping(second, first)
+}
+
+func parsePair(assignmentPair string) (sectionAssignment, sectionAssignment) {
+	assignments := strings.Split(assignmentPair, ",")
+
+	firstAssignment := parseAssignment(assignments[0])
+	secondAssignment := parseAssignment(assignments[1])
+
+	return firstAssignment, secondAssignment
+}
+
+func parseAssignment(assignment string) sectionAssignment {
+	sections := strings.Split(assignment, "-")
+
+	start, _ := strconv.Atoi(sections[0])
+	end, _ := strconv.Atoi(sections[1])
+
+	return sectionAssignment{start: start, end: end}
+}
+
+func isOverlapping(superSet, set sectionAssignment) bool {
+	return superSet.start <= set.start && superSet.end >= set.end
 }
