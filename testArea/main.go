d05f161ec5e5ba12353a3b8d4064cd2ea3983f1d
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-05 21:12:58 +0100

    Get the stack right (in the test area)

--- testArea/main.go
+++ testArea/main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 )
 
+type stack []byte
+
+
 func main() {
-	bytes := []byte{6, 8, 3}
+	bytes := stack{6, 8, 3}
 	fmt.Println("original bytes: ", bytes)
 
 	bytes, b := pop(bytes)
@@ -17,15 +20,15 @@ func main() {
 	fmt.Println("bytes: ", bytes)
 }
 
-func pop(bytes []byte) ([]byte, byte) {
+func push(bytes stack, item byte) stack {
+	return append(bytes, item)
+}
+
+func pop(bytes stack) (stack, byte) {
 	n := len(bytes)
 	ret := bytes[n-1]
 
 	bytes = bytes[:n-1]
 
 	return bytes, ret
-}
-
-func push(bytes []byte, item byte) []byte {
-	return append(bytes, item)
 }
