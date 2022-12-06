873e75c590bdf5e1c0be42d712d6bcc0787aa673
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-06 21:32:39 +0100

    Clear out testArea

--- testArea/main.go
+++ testArea/main.go
@@ -1,24 +1,4 @@
 package main
 
-import "golang.org/x/tour/pic"
-
-func Pic(dx, dy int) [][]uint8 {
-	// pic := make([]uint8, dy)
-	// for i := 0; i < dx; 
-	// var pic [][]uint8
-
-	// return pic
-	ss := make([][]uint8, dy)
-	for y := 0; y < dy; y++ {
-		s := make([]uint8, dx)
-		for x := 0; x < dx; x++ {
-			s[x] = uint8((x + y) / 2)
-		}
-		ss[y] = s
-	}
-	return ss
-}
-
 func main() {
-	pic.Show(Pic)
 }
