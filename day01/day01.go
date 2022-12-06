3bdba266f2a6a7caee2fed1b9d242792ab4a2965
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-06 21:58:53 +0100

    Simplify day 01

--- day01/day01.go
+++ day01/day01.go
@@ -8,22 +8,24 @@ import (
 )
 
 func FindMostCalories(data string) int {
-	parts := strings.Split(data, "\n")
-
-	max := 0
-	sum := 0
-
-	for _, s := range parts {
-		if s == "" {
-			max = Max(max, sum)
-			sum = 0
-		} else {
-			i, _ := strconv.Atoi(s)
-			sum += i
-		}
+	elvesInventory := strings.Split(data, "\n\n")
+	maxCalories := 0
+	for _, ev := range elvesInventory {
+		maxCalories = max(maxCalories, calculateTotalCalories(ev))
 	}
 
-	return max
+	return maxCalories
+}
+
+func calculateTotalCalories(elfInventory string) int {
+	calories := strings.Split(elfInventory, "\n")
+	sum := 0
+	for _, c := range calories {
+		cal, _ := strconv.Atoi(c)
+		sum += cal
+	}
+
+	return sum
 }
 
 func FindTop3MostCalories(data string) int {
@@ -66,7 +68,7 @@ func printTotals(totals []int) {
 	}
 }
 
-func Max(x, y int) int {
+func max(x, y int) int {
 	if x > y {
 		return x
 	} else {
