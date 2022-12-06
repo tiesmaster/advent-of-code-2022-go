19b698dce5bf516aadf988dcf590d685862d6eff
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-06 21:14:17 +0100

    Fiddle around with slices

--- testArea/main.go
+++ testArea/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	fmt.Println("When's Saturday?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
-	default:
-		fmt.Println("Too far away.")
-	}}
+	b := [5]int{1, 2, 3, 4, 5}
+
+	s := b[2:]
+	printSlice(s)
+
+	s = append(s, 99)
+	printSlice(s)
+
+	s[0] = 666
+
+	printSlice(s)
+
+	fmt.Println(b)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
