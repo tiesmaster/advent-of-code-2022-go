cd4f773f0f044fca1f367b2b5ab302a7038abb4b
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-08 22:21:07 +0100

    Work on day 08

--- day08/day08.go
+++ day08/day08.go
@@ -1,5 +1,51 @@
 package day08
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Step01(mapText string) int {
+	mapData := parseMap(mapText)
+	return calculateVisibleTrees(mapData)
+}
+
+func parseMap(mapText string) [][]int {
+	rowCount, columnCount := readDimensions(mapText)
+	m := make([][]int, columnCount)
+
+	// initialize map
+	for i := 0; i < len(m); i++ {
+		m[i] = make([]int, rowCount)
+	}
+
+	readMap(mapText, m)
+
+	return m
+}
+
+func readDimensions(mapText string) (rowCount, columnCount int) {
+	columnCount = strings.Index(mapText, "\n")
+	rowCount = strings.Count(mapText, "\n")
+
+	if mapText[len(mapText)-1] != '\n' {
+		// account for missing newline at the end of the mapText
+		rowCount++
+	}
+
+	return
+}
+
+func readMap(mapText string, mapData [][]int) {
+	lines := strings.Split(mapText, "\n")
+	for i, line := range lines {
+		for j, r := range line {
+			x, _ := strconv.Atoi(string(r))
+			mapData[i][j] = x
+		}
+	}
+}
+
+func calculateVisibleTrees(mapData [][]int) int {
 	panic("unimplemented")
 }
