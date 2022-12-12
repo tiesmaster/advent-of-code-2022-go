38af71f6845fe9149c655bdabab020e35ef2ec7c
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-12 09:18:41 +0100

    Parse the grid data

--- day12/day12.go
+++ day12/day12.go
@@ -1,5 +1,62 @@
 package day12
 
-func Step01(data string) int {
+import (
+	"fmt"
+	"strings"
+)
+
+func Step01(gridData string) int {
+	grid, start, end := parseGrid(gridData)
+
+	fmt.Println(grid, start, end)
+	// do the algorithm
 	panic("unimplemented")
+}
+
+type coordinate struct{ x, y int }
+
+func parseGrid(gridData string) (grid [][]int, start, end coordinate) {
+	rowCount, columnCount := readDimensions(gridData)
+
+	// initialize grid
+	grid = make([][]int, rowCount)
+	for i := 0; i < len(grid); i++ {
+		grid[i] = make([]int, columnCount)
+	}
+
+	start, end = readGrid(gridData, grid)
+	return grid, start, end
+}
+
+func readDimensions(gridData string) (rowCount, columnCount int) {
+	columnCount = strings.Index(gridData, "\n")
+	rowCount = strings.Count(gridData, "\n")
+
+	if gridData[len(gridData)-1] != '\n' {
+		// account for missing newline at the end of the mapText
+		rowCount++
+	}
+
+	return
+}
+
+func readGrid(gridData string, grid [][]int) (start, end coordinate) {
+	lines := strings.Split(gridData, "\n")
+	for i, line := range lines {
+		for j, r := range line {
+			var x int
+			switch r {
+			case 'S':
+				start = coordinate{i, j}
+				x = 0
+			case 'E':
+				end = coordinate{i, j}
+				x = 25
+			default:
+				x = int(r - 'a')
+			}
+			grid[i][j] = x
+		}
+	}
+	return
 }
--- main.go
+++ main.go
@@ -3,18 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var hoi []int
-	hoi = make([]int, 4)
-	hoi[0] = 0
-	hoi[1] = 1
-	hoi[2] = 2
-	hoi[3] = 3
-
-	fmt.Println(hoi)
-	doStuff(hoi)
-	fmt.Println(hoi)
-}
-
-func doStuff(hoi []int) {
-	hoi[0] = 3
+	a := 'z'
+	i := int(a - 'a')
+	fmt.Println(a)
+	fmt.Println(i)
 }
