bed40fcb26e08640a5a4c4e0a993ef6c39ca3023
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-13 21:57:49 +0100

    Fiddle around

--- day13/day13.go
+++ day13/day13.go
@@ -1,5 +1,59 @@
 package day13
 
-func Step01(data string) int {
+import (
+	"strings"
+	"unicode"
+)
+
+func Step01(distressSignal string) int {
+	pairs := parseSignal(distressSignal)
+	indices := indicesRightOrder(pairs)
+	return sum(indices)
+}
+
+type pair [2]string
+
+func parseSignal(distressSignal string) []pair {
+	packetPairs := strings.Split(distressSignal, "\n\n")
+
+	pairs := make([]pair, len(packetPairs))
+	for i, pair := range packetPairs {
+		lines := strings.Split(pair, "\n")
+		pairs[i] = [2]string{lines[0], lines[1]}
+	}
+	return pairs
+}
+
+func indicesRightOrder(pairs []pair) []int {
+	indices := make([]int, 0)
+	for index, pair := range pairs {
+		if compare(pair[0], pair[1]) == 1 {
+			indices = append(indices, index)
+		}
+	}
+	return indices
+}
+
+
+func compare(left, right string) int {
+	switch {
+	case isInt(left[0]) && isInt(right[0]):
+		// case: both integer
+		// x, y := 
+		
+	}
+
 	panic("unimplemented")
+}
+
+func isInt(b byte) bool {
+	return unicode.IsDigit(rune(b))
+}
+
+func sum(indices []int) int{
+	sum := 0
+	for _, index := range indices {
+		sum += index
+	}
+	return sum
 }
