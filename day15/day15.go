cbd1b6e6905df2d596e5a466df47710405a4cea7
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-15 19:57:25 +0100

    Calculate coverages

--- day15/day15.go
+++ day15/day15.go
@@ -8,7 +8,7 @@ import (
 func Step01(report string) int {
 	readings := parseReport(report)
 	targetRow := 10
-	return calculateCoverage(readings, targetRow)
+	return calculateOccupiedPositions(readings, targetRow)
 }
 
 func parseReport(report string) []reading {
@@ -41,12 +41,71 @@ func parseCoordinate(coordinateString string) coordinate {
 
 type reading struct{ sensor, beacon coordinate }
 type coordinate struct{ x, y int }
+type rowCoverage struct{ start, end int }
 
-func calculateCoverage(readings []reading, targetRow int) int {
+func calculateOccupiedPositions(readings []reading, targetRow int) int {
+	coverages := make([]rowCoverage, 0)
+	for _, reading := range readings {
+		if b, coverage := calculateCoverage(reading, targetRow); b {
+			coverages = append(coverages, coverage)
+		}
+	}
 	panic("unimplemented")
+}
+
+func calculateCoverage(reading reading, targetRow int) (bool, rowCoverage) {
+	distanceTotal := manhattenDistance(reading.sensor, reading.beacon)
+	distanceY := distanceToRow(reading.sensor, targetRow)
+	halfCoverage := max(0, distanceTotal - distanceY)
+	if halfCoverage > 0 {
+		sensorX := reading.sensor.x
+		return true, rowCoverage{sensorX - halfCoverage, sensorX + halfCoverage}
+	} else {
+		return false, rowCoverage{0, 0}
+	}
+}
+
+func manhattenDistance(a, b coordinate) int {
+	return distanceX(a, b) + distanceY(a, b)
+}
+
+func distanceX(a, b coordinate) int {
+	return abs(a.x - b.x)
+}
+
+func distanceY(a, b coordinate) int {
+	return abs(a.y - b.y)
+}
+
+func distanceToRow(coordinate coordinate, targetRow int) int {
+	return abs(targetRow - coordinate.y)
 }
 
 func toInt(s string) int {
 	x, _ := strconv.Atoi(s)
 	return x
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	} else {
+		return x
+	}
 }
