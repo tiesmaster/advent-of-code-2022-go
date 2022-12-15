ce200846e2d7fbe0152689f1487d238c2ea4c8cb
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-12-15 20:13:18 +0100

    Calculate the occupied positions

--- day15/day15.go
+++ day15/day15.go
@@ -43,20 +43,47 @@ type reading struct{ sensor, beacon coordinate }
 type coordinate struct{ x, y int }
 type rowCoverage struct{ start, end int }
 
+type boundingBox struct {
+	leftBottom coordinate
+	rightTop   coordinate
+}
+
 func calculateOccupiedPositions(readings []reading, targetRow int) int {
+	// calculate coverages
 	coverages := make([]rowCoverage, 0)
 	for _, reading := range readings {
 		if b, coverage := calculateCoverage(reading, targetRow); b {
 			coverages = append(coverages, coverage)
 		}
 	}
-	panic("unimplemented")
+
+	// calculate dimensions
+	coordinates := getCoordinates(readings)
+	bb := getBoundingBox(coordinates)
+	start, end := bb.leftBottom.x, bb.rightTop.x
+	offset := -1 * start
+	bitmap := make([]bool, end - start)
+
+	for _, c := range coverages {
+		for i := c.start; i < c.end+1; i++ {
+			bitmap[i+offset] = true
+		}
+	}
+
+	// calculate occupied positions
+	var count int
+	for i := 0; i < len(bitmap); i++ {
+		if bitmap[i] {
+			count++
+		}
+	}
+	return count
 }
 
 func calculateCoverage(reading reading, targetRow int) (bool, rowCoverage) {
 	distanceTotal := manhattenDistance(reading.sensor, reading.beacon)
 	distanceY := distanceToRow(reading.sensor, targetRow)
-	halfCoverage := max(0, distanceTotal - distanceY)
+	halfCoverage := max(0, distanceTotal-distanceY)
 	if halfCoverage > 0 {
 		sensorX := reading.sensor.x
 		return true, rowCoverage{sensorX - halfCoverage, sensorX + halfCoverage}
@@ -79,6 +106,39 @@ func distanceY(a, b coordinate) int {
 
 func distanceToRow(coordinate coordinate, targetRow int) int {
 	return abs(targetRow - coordinate.y)
+}
+
+func getCoordinates(readings []reading) []coordinate {
+	coordinates := make([]coordinate, 0)
+	for _, r := range readings {
+		coordinates = append(coordinates, r.sensor)
+		coordinates = append(coordinates, r.beacon)
+	}
+	return coordinates
+}
+
+func getBoundingBox(coordinates []coordinate) boundingBox {
+	bb := coordinates[0].boundingBox()
+	for _, p := range coordinates[1:] {
+		bb = bb.union(p.boundingBox())
+	}
+	return bb
+}
+
+func (a coordinate) boundingBox() boundingBox {
+	return boundingBox{a, a}
+}
+
+func (a boundingBox) union(b boundingBox) boundingBox {
+	return boundingBox{a.leftBottom.min(b.leftBottom), a.rightTop.max(b.rightTop)}
+}
+
+func (a coordinate) min(b coordinate) coordinate {
+	return coordinate{min(a.x, b.x), min(a.y, b.y)}
+}
+
+func (a coordinate) max(b coordinate) coordinate {
+	return coordinate{max(a.x, b.x), max(a.y, b.y)}
 }
 
 func toInt(s string) int {
